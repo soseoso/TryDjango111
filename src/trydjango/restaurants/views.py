800a8ca9d5d0cca648df1e34245d85d7859f6a42
soseoso <soseoso@naver.com>
2018-06-15 17:12:10 +0900

    Context in Django templates

--- src/trydjango/restaurants/views.py
+++ src/trydjango/restaurants/views.py
@@ -6,6 +6,21 @@ from django.shortcuts import render
 # function based view
 
 def home(request):  # Pass an argument as a request
-	num = random.randint(0, 10000000)
-	return render(request, "base.html", {'html_var' : True, 'num': num})  # some sort of responses
-						   # templates   # extra context - dict
+	num = None
+	some_list = [
+		random.randint(0, 10000000), 
+		random.randint(0, 10000000), 
+		random.randint(0, 10000000)
+	]
+	condition_bool_item = True
+	# The logic should actually exist in the view function itself
+	# not so much in the template.
+	if condition_bool_item:
+		num = random.randint(0, 10000000)
+	
+	context = {
+		'num': num, 
+		'some_list': some_list
+	}  # 길어서, context라는 변수에 저장.
+	return render(request, "base.html", context)  # some sort of responses
+						   
