5be32ee02688aa3a389cfa3de2bd533f511dd2ef
soseoso <soseoso@naver.com>
2018-06-15 11:50:10 +0900

    Rendering HTML using f string

--- src/trydjango/restaurants/views.py
+++ src/trydjango/restaurants/views.py
@@ -5,5 +5,17 @@ from django.shortcuts import render
 # function based view
 
 def home(request):  # Pass an argument as a request
-	return HttpResponse("hello")
+	html_var = 'using f strings'
+	html_ = f"""  # python f string <- from python 3.6
+	<!DOCTYPE html>
+	<html lang = en>
+	<head>
+	</head>
+	<body>
+		<h1>Hello, World!</h1>
+		<p> This is html comming through {html_var}</p>
+	</body>
+	</html>
+	"""  # python string
+	return HttpResponse(html_)
 	# return render(request, "home.html", {})  # some sort of responses
