bb2cfedc0fb1c8c47cbedab36e23919153d0ba46
soseoso <soseoso@naver.com>
2018-06-16 11:12:50 +0900

    Template Inheritance

--- src/trydjango/restaurants/views.py
+++ src/trydjango/restaurants/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 # function based view
-
-def home(request):  # Pass an argument as a request
+def home(request):  
 	num = None
 	some_list = [
 		random.randint(0, 10000000), 
@@ -13,14 +12,22 @@ def home(request):  # Pass an argument as a request
 		random.randint(0, 10000000)
 	]
 	condition_bool_item = True
-	# The logic should actually exist in the view function itself
-	# not so much in the template.
+
 	if condition_bool_item:
 		num = random.randint(0, 10000000)
 	
 	context = {
 		'num': num, 
 		'some_list': some_list
-	}  # 길어서, context라는 변수에 저장.
-	return render(request, "base.html", context)  # some sort of responses
+	}  
+	return render(request, "home.html", context)  
 						   
+def about(request):  
+	context = {
+	} 
+	return render(request, "about.html", context) 
+
+def contact(request):  
+	context = {
+	} 
+	return render(request, "contact.html", context)  
