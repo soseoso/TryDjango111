7e7623def4351d9e7876f6dae37dc1de97d8b31e
ThanksForSharing <soseoso@naver.com>
2018-09-12 12:54:50 +0900

    25 - The Power Of Django Forms

--- src/trydjango/restaurants/forms.py
+++ src/trydjango/restaurants/forms.py
@@ -4,4 +4,9 @@ class RestaurantCreateForm(forms.Form):
     name            = forms.CharField()
     location        = forms.CharField(required='True')
     category        = forms.CharField()
-   
+    
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if name == 'Hello':
+            raise forms.ValidationError('Not a valid name')
+        return name
--- src/trydjango/restaurants/views.py
+++ src/trydjango/restaurants/views.py
@@ -9,20 +9,22 @@ from .forms import RestaurantCreateForm
 from .models import RestaurantLocation
 
 def restaurant_createview(request):
-	# if request.method == 'GET':
-	# 	print('get data')
-
-	if request.method == 'POST':
-		print('post data')
-		print(request.POST)
-		title = request.POST.get('title') # request.POST['title']
-		location = request.POST.get('location')
-		category = request.POST.get('category')
-		obj = RestaurantLocation.objects.create(name = title, location=location, category=category)
-
+	form = RestaurantCreateForm(request.POST or None)
+	errors = None
+	if form.is_valid():
+		obj = RestaurantLocation.objects.create(
+			name = form.cleaned_data.get('name'), 
+			location = form.cleaned_data.get('location'), 
+			category = form.cleaned_data.get('category')
+		)
 		return HttpResponseRedirect('/restaurants/')
+		
+	if form.errors:
+		print(form.errors)
+		errors = form.errors
+
 	template_name = 'restaurants/form.html'
-	context = {}
+	context = {'form': form}
 	return render(request, template_name, context)
 
 def restaurant_listview(request):
